62d5dd6c04d904f4175c33860d7cc9005ff390f5
basox70 <basox70@users.noreply.github.com>
2018-03-29 18:38:43 +0200

    Create branch for sqlite DB

--- src/com/aiproject/RWDatas.java
+++ src/com/aiproject/RWDatas.java
@@ -11,7 +11,9 @@ import flexjson.JSONSerializer;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -19,11 +21,11 @@ import java.util.Map;
 public class RWDatas {
 
     private FileReader database;
-    private String fileName = System.getenv("APPDATA") + "\\Char_Recognition\\data.json";
+    private String fileName = System.getenv("APPDATA") + "\\Char_Recognition\\data.db";
 
-    public void toJSON(String character, double[] inputs, Map<Double, double[]> map) {
-        verifyFile();
-        Object datas =  fromJSON();
+    public void toDB(String character, double[] inputs, Map<Double, double[]> map) {
+        verifyDB();
+        Object datas =  fromDB();
         try {
             // TODO output weights in json
 
@@ -72,14 +74,14 @@ public class RWDatas {
         }
     }
 
-    public double[] fromJSON(char character) {
-        verifyFile();
+    public double[] fromDB(char character) {
+        verifyDB();
 
         return null;
     }
 
-    public Object fromJSON() {
-        verifyFile();
+    public Object fromDB() {
+        verifyDB();
         try {
             database = new FileReader(fileName);
             JSONDeserializer parser = new JSONDeserializer();
@@ -95,15 +97,20 @@ public class RWDatas {
     /**
      * Function used to verify/create the data file
      */
-    private void verifyFile() {
+    private void verifyDB() {
         File file = new File(fileName);
         if (!file.exists()) {
             file.getParentFile().mkdirs();
-            try {
-                file.createNewFile();
-            } catch (IOException e1) {
+            String url = "jdbc:sqlite:" + fileName;
+            try (Connection conn = DriverManager.getConnection(url)) {
+                if (conn != null) {
+                    DatabaseMetaData meta = conn.getMetaData();
+                    System.out.println("The driver name is " + meta.getDriverName());
+                    System.out.println("A new database has been created.");
+                }
+            } catch (Exception e1) {
                 System.out.println("Could not create file");
-                //e1.printStackTrace();
+                e1.printStackTrace();
             }
         }
     }
